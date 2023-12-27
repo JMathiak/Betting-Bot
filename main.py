17de02394229f7d00f5b6e28216fd973bc6d3613
Josh Mathiak <jmathiak4@gmail.com>
2023-12-27 13:44:20 -0600

    restructured approach to commands

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
-import bot
+import settings
+import discord
+from discord.ext import commands
+
+logger = settings.logging.getLogger("bot")
+
+
+def run():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+    @bot.event
+    async def on_ready():
+        logger.info(f"UserL {bot.user} (ID: {bot.user.id})")
+
+    @bot.command(
+        aliases=['p'],
+        help="This is help",
+        description="This is description",
+        brief="This is brief"
+    )
+    async def ping(ctx):
+        """Answers with pong"""
+        await ctx.send("pong")
+
+    @bot.command()
+    async def say(ctx, *what):
+        """Answers with pong"""
+        await ctx.send(" ".join(what))
+
+    bot.run(settings.DISCORD_TOKEN, root_logger=True)
 
 if __name__ == '__main__':
-    bot.run_discord_bot()
-    
+    run()
